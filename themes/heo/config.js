6396be083f0c5dce5dcdc9c4bee38f66ab6e8bdb
tonyeric0317 <tonyeric0317@gmail.com>
2026-03-08 01:51:11 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -37,7 +37,7 @@ const CONFIG = {
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推薦文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
-  // 右侧个人资料卡牌欢迎语，点击可自动切换
+  /*// 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
     '🔍 股票',
@@ -45,7 +45,7 @@ const CONFIG = {
     '🏃 閱讀',
     '🏠 運動',
     '🤖️ 煮飯',
-  ],
+  ],*/
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
@@ -115,11 +115,11 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  /*HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群討論分享',
   HEO_SOCIAL_CARD_TITLE_3: '點擊加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',*/
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章數:',
