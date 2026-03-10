7b40d62ddddb42b7e9c3d57dccd4d8929b981502
tonyeric0317 <tonyeric0317@gmail.com>
2026-03-10 21:41:36 +0800

    Update external scripts and styles configuration

--- blog.config.js
+++ blog.config.js
@@ -49,9 +49,12 @@ const BLOG = {
   ...require('./conf/notion.config'), // 读取notion数据库相关的扩展配置，例如自定义表头
   ...require('./conf/dev.config'), // 开发、调试时需要关注的配置
 
-  // 自定义外部脚本，外部样式
-  CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
-  CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
+// 自定义外部脚本，外部样式
+  CUSTOM_EXTERNAL_JS: [
+    'data:text/javascript,var s=document.createElement("style");s.innerHTML="#today-card{pointer-events:none !important;cursor:default !important;}";document.head.appendChild(s);'
+  ],
+  CUSTOM_EXTERNAL_CSS: [''],
+
 
   // 自定义菜单
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
