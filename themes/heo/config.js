f95e59d789260a59e9b5932f18229024db6e1c5d
tonyeric0317 <tonyeric0317@gmail.com>
2026-03-10 08:30:14 +0800

    Disable HEO social card settings
    Comment out social card configuration options.

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -116,11 +116,12 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群討論分享',
-  HEO_SOCIAL_CARD_TITLE_3: '點擊加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  // HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  // HEO_SOCIAL_CARD_TITLE_1: '交流',
+  // HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群討論分享',
+  // HEO_SOCIAL_CARD_TITLE_3: '點擊加入社群',
+  // HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章數:',
